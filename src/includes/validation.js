28a1556049dcc86b8345aad6dcfab155b8367b25
Imran189 <imranimu189@gmail.com>
2022-10-15 23:33:17 +0600

    form validation

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,11 @@
-import { Form as veeFrom, Field as veeField } from "vee-validate";
+import { Form as veeForm, Field as veeField, defineRule } from "vee-validate";
+import { required } from "@vee-validate/rules";
 
 export default {
   install(app) {
-    app.component("veeFrom", veeFrom);
+    app.component("veeForm", veeForm);
     app.component("veeField", veeField);
+
+    defineRule("required", required);
   },
 };
